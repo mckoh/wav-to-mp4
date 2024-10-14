e0011390d72b3e44ccc6a1b1069a263d453e8131
mckoh <michael.kohlegger@gmail.com>
2024-10-14 21:38:47 +0200

    prepared app for docker

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 from mp3_to_mp4 import mp3_to_mp4
 from tempfile import NamedTemporaryFile
 from os import remove
-
-remove("test.mp4")
+from os.path import isfile
 
 st.set_page_config(
     page_icon="🤖",
@@ -19,9 +18,13 @@ sub_title = st.text_input("Sub Title (see demo image)")
 file = st.file_uploader("WAV File", type=["wav"])
 
 if file is not None:
-    with open("test.wav", 'bx') as f:
+    with open("temp.wav", 'bx') as f:
         f.write(file.read())
-    mp3_to_mp4("test.wav", main_title, sub_title)
-    remove("test.wav")
-    with open('test.mp4', 'rb') as f:
-        st.download_button('Download Video', f, file_name='test.mp4')
+
+    if isfile("temp.mp4"):
+        remove("temp.mp4")
+
+    mp3_to_mp4("temp.wav", main_title, sub_title)
+    remove("temp.wav")
+    with open('temp.mp4', 'rb') as f:
+        st.download_button('Download Video', f, file_name='temp.mp4')
