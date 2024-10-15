d9fb6951e6b423bc2520045e07348265d4ad7fa5
mckoh <michael.kohlegger@gmail.com>
2024-10-15 07:10:12 +0200

    added markdown explanation of procedure

--- app.py
+++ app.py
@@ -11,11 +11,16 @@ st.set_page_config(
 
 st.title("MP4 Generator")
 
+st.markdown("**So funktioniert es:** Gib unterhalb einen LV-Titel (main title) und einen Titel für die Einheit (sub title) ein. Lade anschließend ein WAV-Podcast-File hoch. Dieses WAV-File erhältst du z.B. direkt von [Notebook LM](https://notebooklm.google.com/). Sobald du ein File ausgewählt hast, beginnt der Transformationsprozess. Aus dem WAV-File wird nun ein MP4-File erstellt. Wenn dieser Prozess abgeschlossen ist (kann ein paar Minuten dauern) erscheint unterhalb ein **Download-Button**. Nun kannst du dein MP4-File herunterladen.")
+st.markdown("So sieht dein fertiges Video aus:")
+
 st.image("static/demo.png", "Demo image of output file")
 
 main_title = st.text_input("Main Title (see demo image)")
 sub_title = st.text_input("Sub Title (see demo image)")
 file = st.file_uploader("WAV File", type=["wav"])
+
+new_title = main_title.replace(" ", "_")+"___"+sub_title.replace(" ", "_")+".mp4"
 
 if file is not None:
     with open("temp.wav", 'bx') as f:
@@ -26,5 +31,6 @@ if file is not None:
 
     mp3_to_mp4("temp.wav", main_title, sub_title)
     remove("temp.wav")
-    with open('temp.mp4', 'rb') as f:
-        st.download_button('Download Video', f, file_name='temp.mp4')
+
+    with open("temp.mp4", 'rb') as f:
+        st.download_button('Download MP4-File', f, file_name=new_title)
